878fb291e9ea33d4673fb7fdcdc39fc70010c6c6
rezzo <davi.rezzonico@gmail.com>
2015-11-13 15:26:03 +0100

    System

---
+++ app/src/main/java/it/polimi/group11/Controller/System.java
@@ -0,0 +1,16 @@
+package it.polimi.group11.Controller;
+
+/**
+ * Created by rezzo on 13/11/2015.
+ */
+public class System {
+
+    public void newMatch() {
+
+    }
+
+    public void viewStatistics() {
+
+    }
+
+}
