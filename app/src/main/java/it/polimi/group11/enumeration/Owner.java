8a73ba7ec1c1a476afa74c9139e5ab858bb2a248
Jacopo Magni <jacopo.magni@mail.polimi.it>
2015-11-13 16:50:54 +0100

    Add Owner to enumeration Add Cell to model

--- app/src/main/java/it/polimi/group11/enumeration/Owner.java
+++ app/src/main/java/it/polimi/group11/enumeration/Owner.java
@@ -3,5 +3,41 @@ package it.polimi.group11.enumeration;
 /**
  * Created by Jacopo Magni on 13/11/2015.
  */
+
+/**
+ * Every hole of the grid can be cover with a bead, and every bead has an owner.
+ * Specify the possible owner of a bead.
+ */
+
 public enum Owner {
+
+    /**
+     * There is no bead on that hole, so there is no owner.
+     */
+    NONE,
+
+    /**
+     * The owner of the bead is Player One.
+     */
+    PLAYERONE,
+
+    /**
+     * The owner of the bead is Player Two.
+     */
+    PLAYERTWO,
+
+    /**
+     * The owner of the bead is Player Three.
+     */
+    PLAYERTHREE,
+
+    /**
+     * The owner of the bead is Player Four.
+     */
+    PLAYERFOUR;
+
+
+
 }
+
+
