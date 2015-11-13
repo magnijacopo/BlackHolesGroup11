4a9ba907d2d7ddf16c2421eb533dd82d5e5efaa1
Jacopo Magni <jacopo.magni@mail.polimi.it>
2015-11-13 16:00:00 +0100

    Enumeration

---
+++ app/src/main/java/it/polimi/group11/enumeration/Color.java
@@ -0,0 +1,37 @@
+package it.polimi.group11.enumeration;
+
+/**
+ * Created by Jacopo Magni on 13/11/2015.
+ */
+
+/**
+ * Color is the color of the beads that player can use.
+ * {@link #BLACK}
+ * {@link #WHITE}
+ * {@link #GREEN}
+ * {@link #YELLOW}
+ *
+ * @author Jacopo Magni
+ */
+public enum Color {
+
+    /**
+     * Black color
+     */
+    BLACK,
+
+    /**
+     *White color
+     */
+    WHITE,
+
+    /**
+     * Green color
+     */
+    GREEN,
+
+    /**
+     * Yellow color
+     */
+    YELLOW;
+}
