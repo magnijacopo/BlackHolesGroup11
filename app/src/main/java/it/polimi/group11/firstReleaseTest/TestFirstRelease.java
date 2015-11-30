febf7ef44c123c5cdc161c7eeb993654d8e77da9
Jacopo Magni <jacopo.magni@mail.polimi.it>
2015-11-30 23:49:06 +0100

    Modifiche Test

--- app/src/main/java/it/polimi/group11/firstReleaseTest/TestFirstRelease.java
+++ app/src/main/java/it/polimi/group11/firstReleaseTest/TestFirstRelease.java
@@ -10,72 +10,67 @@ import it.polimi.group11.model.Game;
 public class TestFirstRelease {
 
     /**
-     *
+     * {@link Board}
      */
     private Board board;
-
     /**
-     *
+     * {@link Game}
      */
     private Game game;
-
     /**
-     *
+     * Number of players
      */
     int playersNumber;
-
     /**
-     *
+     * Indicates the player of the current round
      */
     String movingPlayer;
-
     /**
-     *
+     * {@link Board#horizontalBarsPosition}
      */
     String horizontalBarsPosition;
-
     /**
-     *
+     * {@link Board#verticalBarsPosition}
      */
     String verticalBarsPosition ;
-
     /**
-     *
+     * The current positions of the beads
      */
     String beadsPosition;
-
     /**
-     *
+     * Indicates if the number of players is correct.
+     * Remains if the number is more than one and less than five, if not gives false
      */
     boolean checkConfigurationPlayers = true;
-
     /**
-     *
+     * Check the moving player.
+     * Remains true if the moving player id is more than one and less than five, if not gives false
      */
     boolean checkConfigurationMovingPlayer = true;
-
     /**
-     *
+     * Indicates if the setting of the bars is correct.
+     * Remains true if the bars positions is more than zero(included) and less than two(included),
+     * if not gives false
      */
     boolean checkConfigurationBars = true;
-
     /**
-     *
+     * Check the number of bits for each players.
+     * Remains true if it's less than 5(included), if not gives false
      */
     boolean checkConfigurationBeads = true;
-
     /**
-     *
+     * Indicates the last Configuration of the board , including setting of the bars,
+     * position of the beads, and situation of the players before the current move
      */
     String lastConfiguration;
-
     /**
-     *
+     * Indicates the configuration of the Board after the current move including setting of the bars,
+     * position of the beads, and situation of the players, but in the case of an
+     * irregularity it returns an error
      */
     String output;
-
     /**
-     *
+     * Indicates the current move
      */
     String move;
 
