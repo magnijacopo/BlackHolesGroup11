4e823775e7c82c7ceafbdf71b19659dee8b3f029
Alex Fantas <alessandrog.fantini@gmail.com>
2015-11-30 14:59:02 +0100

    board 1.1

--- app/src/androidTest/java/it/polimi/group11/model/BoardTest.java
+++ app/src/androidTest/java/it/polimi/group11/model/BoardTest.java
@@ -9,18 +9,53 @@ import static org.junit.Assert.*;
  */
 public class BoardTest {
 
-    @Test
-    public void testPrepareGrid() throws Exception {
-
-    }
 
     @Test
     public void testCheckGrid() throws Exception {
-
+        Board board=new Board();
+        int set[]={0,0,0,0,0,0,0};
+        board.setHorizontalBarsPosition(set);
+        board.setVerticalBarsPosition(set);
+        String grid = board.checkGrid();
+        String expectedGrid="" +
+                "3232323" +
+                "1001221" +
+                "1020100" +
+                "1012101" +
+                "1202000" +
+                "3320210" +
+                "1001012";
+        assertEquals(expectedGrid,grid);
     }
 
     @Test
     public void testNewBeadsPosition() throws Exception {
-
+        String currentBeadsPosition="" +
+                "0000001" +
+                "0000200" +
+                "0000000" +
+                "0000000" +
+                "0001000" +
+                "0100000" +
+                "0000200";
+        String grid="" +
+                "0022330" +
+                "2001100" +
+                "1200230" +
+                "1222000" +
+                "1120023" +
+                "2231100" +
+                "2331100";
+        String expectedBeads="" +
+                "0000000" +
+                "0000200" +
+                "0000000" +
+                "0000000" +
+                "0000000" +
+                "0100000" +
+                "0000200";
+        String nextBeadsPosition;
+        nextBeadsPosition = board.newBeadsPosition(grid, currentBeadsPosition);
+        assertEquals(expectedBeads,nextBeadsPosition);
     }
 }
