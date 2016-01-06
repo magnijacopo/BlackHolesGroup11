970897c8341e6277e622b4306253b22bb8745930
Jacopo Magni <jacopo.magni@mail.polimi.it>
2016-01-06 19:20:28 +0100

    select player activity update

--- app/src/main/java/it/polimi/group11/SelectPlayersActivity.java
+++ app/src/main/java/it/polimi/group11/SelectPlayersActivity.java
@@ -10,8 +10,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 public class SelectPlayersActivity extends AppCompatActivity {
+
+    private int varProva = 2;
+    private TextView provaprova;
+    private static final String MAX_PLAYER = "You have already add the max number of players";
+    private static final String ADD_PLAYER = "Player added";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,12 +26,21 @@ public class SelectPlayersActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        provaprova = (TextView) findViewById(R.id.textViewProva);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(getVarProva() < 4) {
+                    Snackbar.make(view, ADD_PLAYER, Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    incrementNumberPlayers();
+                    provaprova.setText(Integer.toString(getVarProva()));
+                } else {
+                    Snackbar.make(view, MAX_PLAYER, Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                }
             }
         });
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -46,6 +61,18 @@ public class SelectPlayersActivity extends AppCompatActivity {
             return true;
         }else{
             return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public int getVarProva(){
+        return this.varProva;
+    }
+
+    public void incrementNumberPlayers(){
+        if(varProva < 4){
+        varProva++;
+        }else {
+            varProva = 100;
         }
     }
 }
