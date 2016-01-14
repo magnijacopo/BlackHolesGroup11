0675ad0382ecac6a6254d28a13a41d5531309ad0
Jacopo Magni <jacopo.magni@mail.polimi.it>
2016-01-14 10:16:28 +0100

    grafica

--- app/src/main/java/it/polimi/group11/ViewProfileLeaderboardActivity.java
+++ app/src/main/java/it/polimi/group11/ViewProfileLeaderboardActivity.java
@@ -1,8 +1,9 @@
 package it.polimi.group11;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class ViewProfileLeaderboardActivity extends AppCompatActivity {
 
@@ -10,10 +11,11 @@ public class ViewProfileLeaderboardActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile_leaderboard);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
+    public void goToLeaderboard(View vew) {
+        Intent intent = new Intent(this, LeaderboardActivity.class);
+        startActivity(intent);
+    }
 }
