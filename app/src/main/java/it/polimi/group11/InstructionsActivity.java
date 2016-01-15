32d9ee03dd013db3f9a2f756a88f6e240c3f2a07
Jacopo Magni <jacopo.magni@mail.polimi.it>
2016-01-15 13:41:22 +0100

    fixing

--- app/src/main/java/it/polimi/group11/InstructionsActivity.java
+++ app/src/main/java/it/polimi/group11/InstructionsActivity.java
@@ -12,38 +12,38 @@ public class InstructionsActivity extends AppCompatActivity {
 
     TextView textViewInstructions;
 
-    String eng = "textViewInstructions.setText(Html.fromHtml(\"<html>\\n\" +\n" +
-            "                \"<body>\\n\" +\n" +
-            "                \"<html>\\n\" +\n" +
-            "                \"<body>\\n\" +\n" +
-            "                \"<h2>How To Play:</h2>\\n\" +\n" +
-            "                \"<h4> Beads setting: </h4>\\n\" +\n" +
-            "                \"<p> In his turn a player places his bead, this turn are repeated five times, one for each bead of the player. </p>\\n\" +\n" +
-            "                \"<h4> Game: </h4>\\n\" +\n" +
-            "                \"<p> The goal is to drop all the opponents beads into the black hole. To do so the players can move the bars. Each bar can be moved\\n\" +\n" +
-            "                \"    only one position, for example if the bar is completely to the right can be moved only to the center and not completely to the left.\\n\" +\n" +
-            "                \"    The players can not move the bar that are previously moved by the opponents in the same turn. If there are only two players, one player can\\n\" +\n" +
-            "                \"    not move the same bar for more than two consecutive times. </p>\\n\" +\n" +
-            "                \"<h4> Win: </h4>\\n\" +\n" +
-            "                \"<p> The game ends when only one player has beads on the board. </p>\\n\" +\n" +
-            "                \"</body>\\n\" +\n" +
+    String eng = "textViewInstructions.setText(Html.fromHtml(\"<html>\\n\"" +
+            "                \"<body>\\n\"" +
+            "                \"<html>\\n\"" +
+            "                \"<body>\\n\"" +
+            "                \"<h2>How To Play:</h2>\\n\"" +
+            "                \"<h4> Beads setting: </h4>\\n\"" +
+            "                \"<p> In his turn a player places his bead, this turn are repeated five times, one for each bead of the player. </p>\\n\"" +
+            "                \"<h4> Game: </h4>\\n\" " +
+            "                \"<p> The goal is to drop all the opponents beads into the black hole. To do so the players can move the bars. Each bar can be moved\\n\"" +
+            "                \"    only one position, for example if the bar is completely to the right can be moved only to the center and not completely to the left.\\n\"" +
+            "                \"    The players can not move the bar that are previously moved by the opponents in the same turn. If there are only two players, one player can\\n\"" +
+            "                \"    not move the same bar for more than two consecutive times. </p>\\n\"" +
+            "                \"<h4> Win: </h4>\\n\" " +
+            "                \"<p> The game ends when only one player has beads on the board. </p>\\n\"" +
+            "                \"</body>\\n\" " +
             "                \"</html>\" ";
 
-    String ita = "\"<html>\\n\" +\n" +
-            "                \"<body>\\n\" +\n" +
-            "                \"<html>\\n\" +\n" +
-            "                \"<body>\\n\" +\n" +
-            "                \"<h2>Come giocare:</h2>\\n\" +\n" +
-            "                \"<h4> Posizionare i bead: </h4>\\n\" +\n" +
-            "                \"<p> In questo turno ogni giocatore piazza i propri bead, l'azione si ripete cinque volte perchè il player piazza un solo bead per volta.</p>\\n\" +\n" +
-            "                \"<h4> Gioco: </h4>\\n\" +\n" +
+    String ita = "\"<html>\\n\" " +
+            "                <body>\\n\" +\n" +
+            "                \"<html>\\n\" " +
+            "                \"<body>\\n\" " +
+            "                \"<h2>Come giocare:</h2>\\n\" " +
+            "                \"<h4> Posizionare i bead: </h4>\\n\" " +
+            "                \"<p> In questo turno ogni giocatore piazza i propri bead, l'azione si ripete cinque volte perchè il player piazza un solo bead per volta.</p>\\n\" " +
+            "                \"<h4> Gioco: </h4>\\n\" " +
             "                \"<p> L'obiettio del gioco è far cadere tutti i bead avversari nei 'Black Holes'. Per farli cadere i giocatori possono muovere le barre, ogni barra può essere mossa solo di una posizione alla volta.\\n\" +\n" +
-            "                \"    Se per esempio la barra è completamente a destra non potrà essere spostata completamente a sinistra con una sola mossa.\\n\" +\n" +
-            "                \"    I giocatori non possono muovere le barre già mosse da giocatori avversari durante lo stesso giro. Nel caso ci siano due giocatori c'è una regola aggiuntiva,\\n\" +\n" +
-            "                \"    un giocatore non può muovere la stessa barra per più di due turni consecutivi. </p>\\n\" +\n" +
-            "                \"<h4> Vittoria: </h4>\\n\" +\n" +
-            "                \"<p> Il vincitore è il giocatore che riesce a far cadere tutti i bead avversari facendo sopravvivere i suoi. </p>\\n\" +\n" +
-            "                \"</body>\\n\" +\n" +
+            "                \"    Se per esempio la barra è completamente a destra non potrà essere spostata completamente a sinistra con una sola mossa.\\n\"" +
+            "                \"    I giocatori non possono muovere le barre già mosse da giocatori avversari durante lo stesso giro. Nel caso ci siano due giocatori c'è una regola aggiuntiva,\\n\"" +
+            "                \"    un giocatore non può muovere la stessa barra per più di due turni consecutivi. </p>\\n\"" +
+            "                \"<h4> Vittoria: </h4>\\n\" " +
+            "                \"<p> Il vincitore è il giocatore che riesce a far cadere tutti i bead avversari facendo sopravvivere i suoi. </p>\\n\"" +
+            "                \"</body>\\n\" " +
             "                \"</html>\" ";
 
     @Override
