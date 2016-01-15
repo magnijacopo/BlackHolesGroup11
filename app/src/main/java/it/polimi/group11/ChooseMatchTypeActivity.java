0b075016eb18a5ae7bd578b5592746e66bd61266
aless <alessandro1.piola@mail.polimi.it>
2016-01-15 22:06:53 +0100

    musica

--- app/src/main/java/it/polimi/group11/ChooseMatchTypeActivity.java
+++ app/src/main/java/it/polimi/group11/ChooseMatchTypeActivity.java
@@ -1,16 +1,27 @@
 package it.polimi.group11;
 
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 
 public class ChooseMatchTypeActivity extends AppCompatActivity {
+    Button buttonQuickGame;
+    Button buttonCustomizedGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_match_type);
+
+        Typeface myTypeface = Typeface.createFromAsset(getAssets(), "fonts/SignPainter-HouseScript.ttf");
+
+        buttonQuickGame = (Button) findViewById(R.id.button_quick_game);
+        buttonQuickGame.setTypeface(myTypeface);
+        buttonCustomizedGame = (Button) findViewById(R.id.button_customized_game);
+        buttonCustomizedGame.setTypeface(myTypeface);
     }
 
     public void goToMainActivity(View view) {
